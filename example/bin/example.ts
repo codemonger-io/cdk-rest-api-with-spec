5cfe19d2c421dab40b4bf34517559860ed6d00ac
Kikuo Emoto <kemoto@codemonger.io>
2022-06-29 17:49:30 +0900

    chore(example): rename example CDK stack

--- example/bin/example.ts
+++ example/bin/example.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { ExampleStack } from '../lib/example-stack';
 
 const app = new cdk.App();
-new ExampleStack(app, 'ExampleStack', {
+new ExampleStack(app, 'api-with-spec-example', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
