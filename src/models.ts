206ba6c5b890dc5820a531c9021d44fdd35d9e4d
Kikuo Emoto <kemoto@codemonger.io>
2022-07-06 13:03:02 +0900

    docs: refine API doc comments
    - Changes the following types into interfaces so that `api-extractor`
      processes comments on properties,
        - `ModelOptionsWithSpec`
        - `ResourceOptionsWithSpec`
        - `MethodOptionsWithSpec`
        - `MethodResponseWithSpec`
    - Adds and fixes block tags. All components are `@beta` for now.
    - Makes itemizations code blocks because `api-documenter` does not
      properly handle itemizations.

--- src/models.ts
+++ src/models.ts
@@ -6,180 +6,311 @@ import { IAuthorizerWithSpec } from './authorizer';
 import { JsonSchemaEx } from './json-schema-ex';
 
 /**
- * `RestApi` augmented with the features to build the OpenAPI specification.
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.RestApi.html | aws_apigateway.RestApi}
+ * augmented with the features to build the OpenAPI specification.
+ *
+ * @beta
  */
 export interface IRestApiWithSpec extends apigateway.RestApi {
   /**
-   * Underlying `RestApi` object.
+   * Underlying
+   * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.RestApi.html | aws_apigateway.RestApi}
+   * object.
    *
-   * If you directly change this object, the `IRestApiWithSpec` instance cannot
-   * sync with your updates.
+   * @remarks
+   *
+   * If you directly change this object, the {@link IRestApiWithSpec} instance
+   * cannot sync with your updates.
+   *
+   * @beta
    */
   underlying: apigateway.RestApi;
 
   /**
    * Root resource ('/') with the features to build the OpenAPI specification.
+   *
+   * @beta
    */
   root: IBaseResourceWithSpec;
 
-  /** Adds a new model. */
+  /**
+   * Adds a new model.
+   *
+   * @beta
+   */
   addModel(id: string, props: ModelOptionsWithSpec): apigateway.Model;
 }
 
 /**
- * Special interface for the root resource.
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.IResource.html | aws_apigateway.IResource}
+ * augmented with the features to build the OpenAPI specification.
  *
- * This interface is necessary because `RestApi.root` might not satisfy
- * `Resource`.
+ * @remarks
+ *
+ * This interface duplicates {@link IResourceWithSpec} but is necessary because
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.RestApi.html#root | aws_apigateway.RestApi.root}
+ * might not satisfy
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.Resource.html | aws_apigateway.Resource}.
  *
  * It might not be obvious but this interface is a super-interface of
- * `IResourceWithSpec`.
+ * {@link IResourceWithSpec}.
+ *
+ * @beta
  */
 export interface IBaseResourceWithSpec extends apigateway.IResource {
-  /** Default method options with the OpenAPI specification. */
+  /**
+   * Default method options with the OpenAPI specification.
+   *
+   * @beta
+   */
   defaultMethodOptions?: MethodOptionsWithSpec;
 
   /**
    * Parent resource.
+   *
+   * @remarks
+   *
    * `undefined` if this resource represents the root.
+   *
+   * @beta
    */
   parentResource?: IBaseResourceWithSpec;
 
-  /** Adds a new child resource with the OpenAPI specification. */
+  /**
+   * Adds a new child resource with the OpenAPI specification.
+   *
+   * @beta
+   */
   addResource(
     pathPart: string,
     options?: ResourceOptionsWithSpec,
   ): IResourceWithSpec;
 
-  /** Adds a method with the OpenAPI specification. */
+  /**
+   * Adds a method with the OpenAPI specification.
+   *
+   * @beta
+   */
   addMethod(
     httpMethod: string,
-    target?: apigateway.Integration, // TODO: augment the type
+    target?: apigateway.Integration,
     options?: MethodOptionsWithSpec,
-  ): apigateway.Method; // TODO: augment the type
+  ): apigateway.Method;
 }
 
 /**
- * `IResource` augmented with the features to build the OpenAPI specification.
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.Resource.html | aws_apigateway.Resource}
+ * augmented with the features to build the OpenAPI specification.
+ *
+ * @beta
  */
 export interface IResourceWithSpec extends apigateway.Resource {
-  /** Default method options with the OpenAPI specification. */
+  /**
+   * Default method options with the OpenAPI specification.
+   *
+   * @beta
+   */
   defaultMethodOptions?: MethodOptionsWithSpec;
 
-  /** Parent resource. `undefined` if this resource is the root. */
+  /**
+   * Parent resource.
+   *
+   * @remarks
+   *
+   * `undefined` if this resource is the root.
+   *
+   * @beta
+   */
   parentResource?: IBaseResourceWithSpec;
 
-  /** Adds a new child resource with the OpenAPI specification. */
+  /**
+   * Adds a new child resource with the OpenAPI specification.
+   *
+   * @beta
+   */
   addResource(
     pathPart: string,
     options?: ResourceOptionsWithSpec,
   ): IResourceWithSpec;
 
-  /** Adds a method with the OpenAPI specification. */
+  /**
+   * Adds a method with the OpenAPI specification.
+   *
+   * @beta
+   */
   addMethod(
     httpMethod: string,
-    target?: apigateway.Integration, // TODO: augment the type
+    target?: apigateway.Integration,
     options?: MethodOptionsWithSpec,
-  ): apigateway.Method; // TODO: augment the type
+  ): apigateway.Method;
 }
 
 /**
- * `ModelOptions` augmented with the properties necessary to build the OpenAPI
- * specification.
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.ModelOptions.html | aws_apigateway.ModelOptions}
+ * augmented with the properties necessary to build the OpenAPI specification.
+ *
+ * @remarks
  *
  * Has an extended `schema`.
- */
-export type ModelOptionsWithSpec = Omit<apigateway.ModelOptions, 'schema'> & {
-  /** Extended schema definition. */
-  schema: JsonSchemaEx;
-};
-
-/**
- * `ResourceOptions` augmented with the properties necessary to build the
- * OpenAPI specification.
- */
-export type ResourceOptionsWithSpec = Omit<apigateway.ResourceOptions, 'defaultMethodOptions'> & {
-  /** Default method options with the OpenAPI specification. */
-  defaultMethodOptions?: MethodOptionsWithSpec;
-};
-
-/**
- * `MethodOptions` augmented with the properties necessary to build the OpenAPI
- * specification.
  *
- * `operationName` corresponds to `paths[path][method].operationId`.
+ * @beta
  */
-export type MethodOptionsWithSpec = Omit<apigateway.MethodOptions, 'authorizer' | 'methodResponses'> & {
-  /** Authorizer augmented with the OpenAPI specification. */
+export interface ModelOptionsWithSpec extends apigateway.ModelOptions {
+  /**
+   * Extended schema definition.
+   *
+   * @beta
+   */
+  schema: JsonSchemaEx;
+}
+
+/**
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.ResourceOptions.html | aws_apigateway.ResourceOptions}
+ * augmented with the properties necessary to build the OpenAPI specification.
+ *
+ * @beta
+ */
+export interface ResourceOptionsWithSpec extends apigateway.ResourceOptions {
+  /**
+   * Default method options with the OpenAPI specification.
+   *
+   * @beta
+   */
+  defaultMethodOptions?: MethodOptionsWithSpec;
+}
+
+/**
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.MethodOptions.html | aws_apigateway.MethodOptions}
+ * augmented with the properties necessary to build the OpenAPI specification.
+ *
+ * @remarks
+ *
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.MethodOptions.html#operationname | operationName}
+ * corresponds to
+ * {@link https://spec.openapis.org/oas/latest.html#operation-object | paths[path][method].operationId}
+ * in the OpenAPI specification.
+ *
+ * @beta
+ */
+export interface MethodOptionsWithSpec extends apigateway.MethodOptions {
+  /**
+   * Authorizer augmented with the OpenAPI specification.
+   *
+   * @beta
+   */
   authorizer?: IAuthorizerWithSpec;
   /**
    * Summary of the method.
    *
-   * Corresponds to `paths[path][method].summary` in the OpenAPI specification.
+   * @remarks
+   *
+   * Corresponds to
+   * {@link https://spec.openapis.org/oas/latest.html#operation-object | paths[path][method].summary}
+   * in the OpenAPI specification.
+   *
+   * @beta
    */
   summary?: string;
   /**
    * Description of the method.
    *
-   * Corresponds to `paths[path][method].description` in the OpenAPI
-   * specification.
+   * @remarks
+   *
+   * Corresponds to
+   * {@link https://spec.openapis.org/oas/latest.html#operation-object | paths[path][method].description}
+   * in the OpenAPI specification.
+   *
+   * @beta
    */
   description?: string;
   /**
    * Request parameters which maps parameter objects for the OpenAPI
    * specification instead of boolean values.
    *
-   * Corresponds to `paths[path][method].parameters` in the OpenAPI
-   * specification.
+   * @remarks
    *
-   * Possible keys are the same as those of `requestParameters`; i.e., in one of
-   * the following forms,
-   * - `method.request.path.<parameter-name>`
-   * - `method.request.querystring.<parameter-name>`
-   * - `method.request.header.<parameter-name>`
+   * Corresponds to
+   * {@link https://spec.openapis.org/oas/latest.html#operation-object | paths[path][method].parameters}
+   * in the OpenAPI specification.
+   *
+   * Possible keys are the same as those of
+   * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.MethodOptions.html#requestparameters | requestParameters};
+   * i.e., in one of the following forms,
+   *
+   * ```
+   * - method.request.path.<parameter-name>
+   * - method.request.querystring.<parameter-name>
+   * - method.request.header.<parameter-name>
+   * ```
    *
    * A key represents the following properties of the parameter object of the
    * OpenAPI specification,
-   * - `name` = `<parameter-name>`
-   * - `in`
-   *     - `path` for `method.request.path.*`
-   *     - `query` for `method.request.querystirng.*`
-   *     - `header` for `method.request.header.*`
+   *
+   * ```
+   * - name = <parameter-name>
+   * - in
+   *   "path" for "method.request.path.*",
+   *   "query" for "method.request.querystirng.*",
+   *   "header" for "method.request.header.*".
+   * ```
    *
    * Values of `required` properties are copied to corresponding boolean values
-   * of `requestParameters`.
+   * of
+   * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.MethodOptions.html#requestparameters | requestParameters}.
    *
-   * If both `requestParameters` and `requestParameterSchemas` are specified,
-   * `requestParameterSchemas` precedes.
+   * If both
+   * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.MethodOptions.html#requestparameters | requestParameters}
+   * and `requestParameterSchemas` are specified, `requestParameterSchemas`
+   * precedes.
+   *
+   * @beta
    */
   requestParameterSchemas?: { [key: string]: BaseParameterObject };
   /**
    * Method responses augmented with properties necessary for the OpenAPI
    * specification.
+   *
+   * @beta
    */
   methodResponses?: MethodResponseWithSpec[],
-};
+}
 
 /**
- * `MethodResponse` augmented with properties necessary for the OpenAPI
- * specification.
+ * {@link https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway.MethodResponse.html | aws_apigateway.MethodResponse}
+ * augmented with properties necessary for the OpenAPI specification.
+ *
+ * @beta
  */
-export type MethodResponseWithSpec = apigateway.MethodResponse & {
-  /** Description of the response. */
+export interface MethodResponseWithSpec extends apigateway.MethodResponse {
+  /**
+   * Description of the response.
+   *
+   * @beta
+   */
   description?: string;
-};
+}
 
-/** Parsed request or response parameter key. */
+/**
+ * Parsed request or response parameter key.
+ *
+ * @beta
+ */
 export class ParameterKey {
   constructor(
+    /** Request or response. */
     readonly direction: 'request' | 'response',
+    /** Name of the parameter. */
     readonly name: string,
+    /** Location of the parameter. */
     readonly location: 'path' | 'query' | 'header',
+    /** Whether the parameter can have multiple values. */
     readonly explode: boolean,
   ) {}
 
   /**
    * Parses a given request or response parameter key.
+   *
+   * @remarks
    *
    * A parameter key must have the following form,
    *
@@ -190,11 +321,14 @@ export class ParameterKey {
    * `<direction>` must be either `request` or `response`.
    *
    * `<location>` must be one of the following,
-   * - `path`
-   * - `querystring` (invalid if direction=response)
-   * - `multivaluequerystring` (invalid direction=response)
-   * - `header`
-   * - `multivalueheader`
+   *
+   * ```
+   * - "path"
+   * - "querystring" (invalid if direction=response)
+   * - "multivaluequerystring" (invalid direction=response)
+   * - "header"
+   * - "multivalueheader"
+   * ```
    *
    * @param key
    *
@@ -203,6 +337,8 @@ export class ParameterKey {
    * @throws RangeError
    *
    *   If `key` is not a valid parameter key.
+   *
+   * @beta
    */
   static parseParameterKey(key: string): ParameterKey {
     const match = key.match(/^method\.(request|response)\.(path|querystring|multivaluequerystring|header|multivalueheader)\.(.+)/);
